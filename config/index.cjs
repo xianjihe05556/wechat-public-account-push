3e2afc61343ab544b13c717560c6115b13d0fdad
xianjihe05556 <144887399+xianjihe05556@users.noreply.github.com>
2023-09-13 16:06:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'otASg6aLDothT2BbKJlxJpBNgGF0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'QghL8Gu-mAeFoMYGiFekRL_95b5y1PfMbQ8SgHU396o',
+      useTemplateId: '4ust9Iohzz-kR-AxNZwsv1OAneeZBvXt6E0_pLrD_mU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '4-12',
       festivals: [
