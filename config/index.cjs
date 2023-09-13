f964de6c93c4ab2017f54991c37a110598f593f9
xianjihe05556 <144887399+xianjihe05556@users.noreply.github.com>
2023-09-13 16:02:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '宝贝', year: '1996', date: '04-12',
+          type: '生日', name: '宝贝', year: '2002', date: '04-12',
         },
         {
           type: '节日', name: '相识纪念日', year: '2023', date: '03-27',
@@ -42,9 +42,9 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-03-27' },
+        { keyword: 'love_day', date: '2023-03-27' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-03-27' },
+        { keyword: 'marry_day', date: '2023-03-27' },
       ],
     },
   ],
